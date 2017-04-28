684c8a908ced328fd80a244dae27d419b3b9befd
William KLEIN <wklein@infinitesquare.com>
2017-04-29 00:33:10 +0200

    update gitignore

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,4 @@ import { Game } from './game/game';
 import { Vector } from './physics/vector';
 
 var game = new Game();
-game.init(10, 3000, new Vector(100, 70), { width: 200, height: 900 }, 900, 300);
-(<any>window).game = game;
-(<any>window).Vector = Vector;
+game.init(10, 3000, new Vector(100, 70), { width: 200, height: 900 }, 900, 3);
